4f71fda48e099e5d7dd464500ef177dec34d973a
Matthias Eymers <matthias.eymers@haspa-next.de>
2018-03-12 09:52:27 +0100

    gradle build fix  - added support libs

--- app/src/main/java/com/example/matthias/postfitty/Activity/UserProfileActivity.kt
+++ app/src/main/java/com/example/matthias/postfitty/Activity/UserProfileActivity.kt
@@ -8,6 +8,6 @@ class UserProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_userprofile)
+        setContentView(R.layout.activity_user_profile)
     }
 }
--- app/src/main/java/com/example/matthias/postfitty/Model/Post.kt
+++ app/src/main/java/com/example/matthias/postfitty/Model/Post.kt
@@ -4,5 +4,4 @@ import java.util.*
 
 class Post(var id: Int, var message: String, var latitude: Double, var longitude: Double, var created_at: Date) {
 
-    
 }
