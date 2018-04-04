974d2255bf2fdbc8d935f2700c0456d41eb05aa5
Matthias Eymers <matthias.eymers@haspa-next.de>
2018-04-04 17:46:33 +0200

    working map fragment

--- app/src/main/java/com/example/matthias/postfitty/Fragments/MapsFragment.kt
+++ app/src/main/java/com/example/matthias/postfitty/Fragments/MapsFragment.kt
@@ -8,29 +8,32 @@ import android.view.ViewGroup
 import com.example.matthias.postfitty.Activity.SplashActivity
 import com.example.matthias.postfitty.Model.Post
 import com.example.matthias.postfitty.R
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapFragment
+import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 
 class MapsFragment : Fragment(), OnMapReadyCallback {
-
-    private lateinit var mMap: GoogleMap
     private var posts = SplashActivity.posts
+
+    private var mapView: MapView? = null
+    private lateinit var mMap: GoogleMap
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-       return inflater.inflate(R.layout.fragment_maps, container, false)
+        return inflater.inflate(R.layout.fragment_maps, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val fragment = childFragmentManager.findFragmentById(R.id.gMap)
-        fragment.getMapAsync(this)
+        mapView = view.findViewById(R.id.map) as MapView?
+        mapView?.onCreate(savedInstanceState)
+        mapView?.onResume()
+        mapView?.getMapAsync(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -39,6 +42,8 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
         for (post: Post in posts) {
             mMap.addMarker(MarkerOptions().position(LatLng(post.latitude, post.longitude)).title(post.title))
         }
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(posts[0].latitude, posts[0].longitude), 8f))
     }
 
     companion object {
